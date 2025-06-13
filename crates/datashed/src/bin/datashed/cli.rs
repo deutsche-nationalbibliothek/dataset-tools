a52e1f1ca8af8ad13dd337d33bf0f421be9508fa
Nico Wagner <n.wagner@dnb.de>
2025-06-13 09:24:35 +0000

    feat(datashed): initialize global thread pool
    Signed-off-by: Nico Wagner <n.wagner@dnb.de>

--- crates/datashed/src/bin/datashed/cli.rs
+++ crates/datashed/src/bin/datashed/cli.rs
@@ -15,6 +15,12 @@ const STYLES: Styles = Styles::styled()
 #[command(max_term_width = 72, styles = STYLES)]
 #[command(disable_help_subcommand = true)]
 pub(crate) struct Args {
+    /// Number of threads to use. If this options isn't set or a value
+    /// of "0" is chosen, the maximum number of available threads
+    /// is used.
+    #[clap(short = 'j', long, hide_env_values = true)]
+    pub(crate) num_jobs: Option<usize>,
+
     #[command(subcommand)]
     pub(crate) cmd: Box<Command>,
 }
