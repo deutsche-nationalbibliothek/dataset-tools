7c61366eff428567d2026320fd17ceaef4ab1adf
Nico Wagner <n.wagner@dnb.de>
2026-05-05 12:18:25 +0000

    feat(datashed): Reintroduce *-thesis doctypes
    Signed-off-by: Nico Wagner <n.wagner@dnb.de>

--- crates/datashed/src/doctype.rs
+++ crates/datashed/src/doctype.rs
@@ -8,18 +8,22 @@ use crate::refinement::Refinements;
 
 pub type DoctypeRefinements = Refinements<Doctype>;
 
-pub(crate) const DOCTYPES: [&str; 26] = [
+pub(crate) const DOCTYPES: [&str; 30] = [
     "abstract",
+    "bachelor-thesis",
     "blog-post",
     "blurb",
     "conference-object",
     "conference-paper",
     "conference-proceedings",
     "correction",
+    "diploma-thesis",
     "doctoral-thesis",
     "journal-article",
     "journal-object",
     "letter-to-the-editor",
+    "magister-thesis",
+    "master-thesis",
     "musical-notation",
     "none",
     "nonfiction-book",
@@ -37,24 +41,25 @@ pub(crate) const DOCTYPES: [&str; 26] = [
     "working-paper",
 ];
 
-#[derive(
-    Debug, Default, Clone, Copy, PartialEq, Serialize, Deserialize,
-)]
+#[derive(Debug, Clone, Copy, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "kebab-case")]
 pub enum Doctype {
     Abstract,
+    BachelorThesis,
     BlogPost,
     Blurb,
     ConferenceObject,
     ConferencePaper,
     ConferenceProceedings,
     Correction,
+    DiplomaThesis,
     DoctoralThesis,
     JournalArticle,
     JournalObject,
     LetterToTheEditor,
+    MagisterThesis,
+    MasterThesis,
     MusicalNotation,
-    #[default]
     None,
     NonfictionBook,
     Other,
@@ -69,6 +74,12 @@ pub enum Doctype {
     TableOfContents,
     Textbook,
     WorkingPaper,
+}
+
+impl Default for Doctype {
+    fn default() -> Self {
+        Self::None
+    }
 }
 
 impl FromStr for Doctype {
@@ -86,26 +97,28 @@ impl FromStr for Doctype {
 }
 
 impl Display for Doctype {
+    #[rustfmt::skip]
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Self::Abstract => write!(f, "abstract"),
+            Self::BachelorThesis => write!(f, "bachelor-thesis"),
             Self::BlogPost => write!(f, "blog-post"),
             Self::Blurb => write!(f, "blurb"),
             Self::ConferenceObject => write!(f, "conference-object"),
             Self::ConferencePaper => write!(f, "conference-paper"),
-            Self::ConferenceProceedings => {
-                write!(f, "conference-proceedings")
-            }
+            Self::ConferenceProceedings => write!(f, "conference-proceedings"),
             Self::Correction => write!(f, "correction"),
+            Self::DiplomaThesis => write!(f, "diploma-thesis"),
             Self::DoctoralThesis => write!(f, "doctoral-thesis"),
             Self::JournalArticle => write!(f, "journal-article"),
             Self::JournalObject => write!(f, "journal-object"),
-            Self::LetterToTheEditor => {
-                write!(f, "letter-to-the-editor")
-            }
+            Self::LetterToTheEditor => write!(f, "letter-to-the-editor"),
+            Self::MagisterThesis => write!(f, "magister-thesis"),
+            Self::MasterThesis => write!(f, "master-thesis"),
             Self::MusicalNotation => write!(f, "musical-notation"),
             Self::None => write!(f, "none"),
             Self::NonfictionBook => write!(f, "nonfiction-book"),
+            Self::Other => write!(f, "other"),
             Self::PolicyPaper => write!(f, "policy-paper"),
             Self::Preface => write!(f, "preface"),
             Self::PreprintArticle => write!(f, "preprint-article"),
@@ -117,7 +130,6 @@ impl Display for Doctype {
             Self::TableOfContents => write!(f, "table-of-contents"),
             Self::Textbook => write!(f, "textbook"),
             Self::WorkingPaper => write!(f, "working-paper"),
-            Self::Other => write!(f, "other"),
         }
     }
 }
